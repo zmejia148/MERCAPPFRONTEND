889f3864d76beaffffc5bb7deef993089d3d4bc9
zmejia148 <zmejia@est.ups.edu.ec>
2025-11-30 17:40:58 -0500

    FRONTEND MERCAPP

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Ajusta según tu API
+  baseURL: 'https://mercappbackend-production.up.railway.app', // Ajusta según tu API
   timeout: 10000,
 });
 
