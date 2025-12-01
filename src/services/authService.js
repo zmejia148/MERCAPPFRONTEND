38601d4184d3db7bc20f79ec52e3a9fe7fa5c0d3
zmejia148 <zmejia@est.ups.edu.ec>
2025-11-30 21:00:32 -0500

    arreglo del api

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,9 @@
+import api from "./api";
+
 export const registerUser = async (data) => {
-  return await api.post("/api/auth/register", data);
+  return await api.post("/auth/register", data); 
 };
 
 export const loginUser = async (data) => {
-  return await api.post("/api/auth/login", data);
+  return await api.post("/auth/login", data);
 };
